2b0b4bd4fd915c8b2097d8fa99f59c2358b7603b
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-01-15 22:05:26 +0100

    #17 Create the hero section in the component folder

---
+++ src/app/components/hero.php
@@ -0,0 +1,23 @@
+<!-- hero section -->
+<div class="h-[600px] overflow-hidden relative">
+    <img src="https://media.graphassets.com/TXa14Ni0Qn6we9ubFqMV" class="object-cover h-full w-full" alt="">
+    <div class="absolute top-0 bottom-0 w-full isolate overflow-hidden bg-gradient-to-b from-transparent to-black px-6 pt-16 shadow-2xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+        <svg viewBox="0 0 1024 1024" class="absolute left-1/2 top-1/2 -z-10 size-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0" aria-hidden="true">
+            <circle cx="512" cy="512" r="512" fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fill-opacity="0.7" />
+            <defs>
+                <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
+                    <stop stop-color="#7775D6" />
+                    <stop offset="1" stop-color="#ef4444" />
+                </radialGradient>
+            </defs>
+        </svg>
+        <div class="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
+            <h2 class="text-balance text-3xl font-semibold tracking-tight text-white sm:text-4xl">Unlock Your Potential with Expert-Led Courses.</h2>
+            <p class="mt-6 text-pretty text-lg/8 text-gray-300">Transform your career and personal growth with our world-class, on-demand courses designed for learners of all levels.</p>
+            <div class="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+                <a href="#" class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">Get started</a>
+                <a href="#" class="text-sm/6 font-semibold text-red-500">Learn more <span aria-hidden="true">→</span></a>
+            </div>
+        </div>
+    </div>
+</div>
