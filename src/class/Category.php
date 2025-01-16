08787e5e5928acdae8133fda9d9c68b5c13d1dca
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-01-16 11:20:42 +0100

    #5 Create the class Category including the attributs, setter and getter

---
+++ src/class/Category.php
@@ -0,0 +1,19 @@
+<?php
+
+class Category {
+    private string $category_name;
+    private string $image;
+
+    public function __construct($category_name, $image){
+        $this->category_name = $category_name;
+        $this->image = $image;
+    }
+    
+    public function __get($name){
+        return $this->$name;
+    }
+
+    public function __set($name, $value){
+        $this->$name = $value;
+    }
+}
