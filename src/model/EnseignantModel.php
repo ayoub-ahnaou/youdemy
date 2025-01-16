42eaf1069c0d2d00eef856d7e1be2f5ba93be4c8
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-01-16 13:05:54 +0100

    #8 Create a function to get all instractors from the database

--- src/model/EnseignantModel.php
+++ src/model/EnseignantModel.php
@@ -11,4 +11,14 @@ class EnseignantModel {
     public function __construct(){
         $this->connecion = Database::getInstance()->getConnection();
     }
+
+    public function getAllInstructors() {
+        try {
+            $stmt = $this->connecion->prepare("SELECT * FROM users WHERE role_id = 2");
+            $stmt->execute();
+            return $stmt->fetchAll();
+        } catch (Exception $e) {
+            throw new Exception("failed get all instractors: " . $e->getMessage());
+        }
+    }
 }
