d95375d5d6881e0aca9cc4e64a16089196910fd7
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-01-16 13:01:59 +0100

    #7 Create a function to update the new fields in the enseignant approuved by the admin

--- src/model/EtudiantModel.php
+++ src/model/EtudiantModel.php
@@ -9,4 +9,25 @@ class EtudiantModel {
     public function __construct(){
         $this->connection = Database::getInstance()->getConnection();
     }
+
+    public function upgradeUserInfos($user_id, $age, $gender, $address, $cin, $specialite, $acad_level, $avatar) {
+        $sql = "UPDATE users 
+        SET age = :age, gender = :gender, address = :address, cin = :cin, specialite = :specialite, academic_level = :academic_level, avatar = :avatar
+        WHERE user_id = :user_id";
+        try {
+            $stmt = $this->connection->prepare($sql);
+            if($stmt->execute([
+                ":age" => $age,
+                ":gender" => $gender,
+                ":address" => $address,
+                ":cin" => $cin,
+                ":specialite" => $specialite,
+                ":academic_level" => $acad_level,
+                ":avatar" => $avatar,
+                ":user_id" => $user_id,
+            ])) return true;
+        } catch (Exception $e) {
+            throw new Exception("Request failed: " . $e->getMessage());
+        }
+    }
 }
