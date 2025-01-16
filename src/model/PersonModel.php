1a2a3402103c84bf44721adb5f68f8de78ead657
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-01-16 13:08:56 +0100

    #6 Create the function of loggin in in the personModel class

--- src/model/PersonModel.php
+++ src/model/PersonModel.php
@@ -12,4 +12,17 @@ class PersonModel {
     public function __construct(){
         $this->connection = Database::getInstance()->getConnection();
     }
+
+    public function login($email, $password) {
+        $sql = "SELECT * FROM users WHERE email = :email";
+        try {
+            $stmt = $this->connection->prepare($sql);
+            $stmt->execute([":email" => $email]);
+            $user = $stmt->fetch();
+            if(!Helpers::verifyPassword($password ,$user["password"])) return false;
+            else return $user;
+        } catch (Exception $e) {
+            throw new Exception("error while logging in: " . $e->getMessage());
+        }
+    }
 }
