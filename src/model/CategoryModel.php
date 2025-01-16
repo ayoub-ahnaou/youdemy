5d61d3ea2b1721f84b4813795ab2b539312d2153
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-01-16 13:18:55 +0100

    #11 Create the class CategorieModel to handle cruds function of the categories

---
+++ src/model/CategoryModel.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\model;
+
+use App\config\Database;
+use Category;
+use Exception;
+use PDO;
+
+class CategoryModel {
+    private PDO $connection;
+
+    public function __construct(){
+        $this->connection = Database::getInstance()->getConnection();
+    }
+}
