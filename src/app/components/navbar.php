ab961a7e8144dc27774a6773569cf735030e8880
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-01-17 22:42:04 +0100

    #18 Make tha navigation bar display the info dynamicaly based on the authentification of the user

--- src/app/components/navbar.php
+++ src/app/components/navbar.php
@@ -1,8 +1,12 @@
+<?php
+include '../../middlewares/access.php';
+?>
+
 <nav class="bg-gray-800 text-white shadow-md sticky top-0 z-10">
     <div class="container mx-auto px-4 flex items-center justify-between h-16">
         <!-- Left Section: Logo -->
         <div class="flex items-center">
-            <a href="http://localhost/youdemy/src/app/pages/index.php" class="text-red-500 text-lg font-bold">Youdemy</a>
+            <a href="./index.php" class="text-red-500 text-lg font-bold">Youdemy</a>
             <span class="text-gray-300 ml-2 text-xs">Your eLearning Platform</span>
         </div>
 
@@ -16,36 +20,44 @@
 
         <!-- Right Section: Icons -->
         <div class="flex max-md:hidden items-center space-x-4">
+            <?php if(!isAuth()) { ?>
             <!-- login Icon -->
             <a href="./login.php" class="flex items-center gap-2">
                 <img src="../assets/svgs/login.svg" class="h-5" alt="">
                 <span class="text-xs">Login</span>
             </a>
+            <?php } ?>
 
+            <?php if(isAuth()) { ?>
             <!-- logout icon -->
-            <a href="./login.php" class="flex items-center gap-2">
+            <a href="../../process/auth/logout.php" class="flex items-center gap-2">
                 <img src="../assets/svgs/logout.svg" class="h-5" alt="">
                 <span class="text-xs">Logout</span>
             </a>
-
+            
             <!-- profile icon -->
-            <a href="./login.php" class="flex items-center gap-2">
+            <a href="./profile.php" class="flex items-center gap-2">
                 <img src="../assets/svgs/profile.svg" class="h-4" alt="">
                 <span class="text-xs">Profile</span>
             </a>
-
+            <?php } ?>
+            
+            <?php if(isAuth() && isAdmin()) { ?>
             <!-- profile icon -->
-            <a href="./login.php" class="flex items-center gap-2">
+            <a href="./dashboard.php" class="flex items-center gap-2">
                 <img src="../assets/svgs/dashboard.svg" class="h-5" alt="">
                 <span class="text-xs">Dashboard</span>
             </a>
+            <?php } ?>
 
+            <?php if(isAuth() && isTeacher()) { ?>
             <!-- notification icon -->
-            <a href="./login.php" class="flex items-center gap-2 relative">
-                <img src="../assets/svgs/notification.svg" class="h-4" alt="">
-                <span class="text-sm">Notifications</span>
-                <span class="bg-pink-500 rounded-full px-1 absolute -top-1 -right-1 text-[10px]">0</span>
+            <a href="./instractor-dashboard.php" class="flex items-center gap-2 relative">
+                <!-- <img src="../assets/svgs/notification.svg" class="h-4" alt=""> -->
+                <span class="text-xs">Dashboard</span>
+                <!-- <span class="bg-pink-500 rounded-full px-1 absolute -top-1 -right-1 text-[10px]">0</span> -->
             </a>
+            <?php } ?>
         </div>
     </div>
 </nav>
