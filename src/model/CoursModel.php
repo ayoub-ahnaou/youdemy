1b26e52652caacecb101e5293e4b0067465de42b
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-01-18 12:42:32 +0100

    #12 Create function to get all courses from database

--- src/model/CoursModel.php
+++ src/model/CoursModel.php
@@ -11,4 +11,17 @@ class CoursModel {
     public function __construct(){
         $this->connection = Database::getInstance()->getConnection();
     }
+
+    public function getAllCourses() {
+        try {
+            $stmt = $this->connection->prepare("SELECT c.*, firstname, lastname, email, gender, category_name 
+                FROM courses c
+                JOIN users u ON u.user_id = c.user_id
+                JOIN categories ca ON ca.category_id = c.category_id");
+            $stmt->execute();
+            return $stmt->fetchAll();
+        } catch (Exception $e) {
+            throw new Exception("failed to retrieve courses: " . $e->getMessage());
+        }
+    }
 }
