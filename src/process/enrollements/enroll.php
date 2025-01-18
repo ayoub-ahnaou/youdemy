bc00552bb0f341e74caf7f89dfe4316916f8b52e
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-01-18 16:28:50 +0100

    #36 Add enrollment processing script for course registration

---
+++ src/process/enrollements/enroll.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\class\Enrollement;
+use App\model\EnrollementModel;
+
+require_once "../../../vendor/autoload.php";
+
+$user_id = $_GET["user_id"];
+$cours_id = $_GET["cours_id"];
+
+$enrollModel = new EnrollementModel();
+$enroll = new Enrollement($user_id, $cours_id);
+
+$enrollModel->enrollNow($enroll);
+header("location: ../../app/pages/details-cours.php?cours_id=$cours_id");
