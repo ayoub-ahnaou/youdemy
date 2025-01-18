ce24f21b019749a55170aa2a7de50b89fc5893ba
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-01-18 13:36:19 +0100

    #13 Add enrollNow method to EnrollementModel for course enrollment

--- src/model/EnrollementModel.php
+++ src/model/EnrollementModel.php
@@ -12,4 +12,19 @@ class EnrollementModel {
     public function __construct(){
         $this->connection = Database::getInstance()->getConnection();
     }
+
+    public function enrollNow(Enrollement $enrollement) {
+        var_dump($enrollement);
+        $sql = "INSERT INTO enrollements (cours_id, user_id) VALUES (:cours_id, :user_id)";
+        try {
+            $stmt = $this->connection->prepare($sql);
+            $res = $stmt->execute([
+                ":cours_id" => $enrollement->__get("cours_id"), 
+                ":user_id" => $enrollement->__get("user_id")
+            ]);
+            return $res;
+        } catch (Exception $e) {
+            throw new Exception("failed to enroll in cours: " . $e->getMessage());
+        }
+    }
 }
