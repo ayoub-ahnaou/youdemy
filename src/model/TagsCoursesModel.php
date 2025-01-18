c4659c19d6d0f4f063e0065cbbbb9fe79f7940bd
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-01-18 16:07:32 +0100

    #27 Add method to assign tags to courses in TagsCoursesModel

--- src/model/TagsCoursesModel.php
+++ src/model/TagsCoursesModel.php
@@ -7,8 +7,21 @@ use PDO;
 
 class TagsCoursesModel {
     private PDO $connection;
-
+    
     public function __construct(){
         $this->connection = Database::getInstance()->getConnection();
+    }
+
+    public function assignTagToCourse($tag_id, $cours_id) {
+        $sql = "INSERT INTO courses_tags (tag_id, cours_id) VALUES (:tag_id, :cours_id)";
+        try {
+            $stmt = $this->connection->prepare($sql);
+            $stmt->execute([
+                ':tag_id' => $tag_id,
+                ':cours_id' => $cours_id,
+            ]);
+        } catch (Exception $e) {
+            throw new Exception("failed to add tag to cours: " . $e->getMessage());
+        }
     }
 }
