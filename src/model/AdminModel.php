9cfd741705b6e3f38d1b6caee69035b1581fba4e
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-01-18 13:33:50 +0100

    #9 Add method to decline enseignant requests in AdminModel

--- src/model/AdminModel.php
+++ src/model/AdminModel.php
@@ -22,4 +22,15 @@ class AdminModel {
         }
     }
 
+    public function declineEnseignantRequest($user_id) {
+        $sql = "UPDATE users SET isRequested = 0, gender = null, cin = null, address = null, age = null, specialite = null, academic_level = null, avatar = null
+        WHERE user_id = :user_id";
+        try {
+            $stmt = $this->connection->prepare($sql);
+            if($stmt->execute([":user_id" => $user_id])) return true;
+        } catch (Exception $e) {
+            throw new Exception("Request failed: " . $e->getMessage());
+        }
+    }
+
 }
