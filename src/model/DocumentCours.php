7ecb839737057894e7b458ba7a7d5f7e3c7fe3cc
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-01-18 12:55:52 +0100

    #12 Create a function to add a new course with a document file type in thier content

--- src/model/DocumentCours.php
+++ src/model/DocumentCours.php
@@ -14,4 +14,27 @@ class DocumentCours extends CoursModel implements CoursInterface {
         $this->connection = Database::getInstance()->getConnection();
     }
 
+    public function createCours(Cours $cours) {
+        $sql = "INSERT INTO courses (title, subtitle, description, image, langues, type, category_id, content, user_id) 
+        VALUES (:title, :subtitle, :description, :image, :langues, :type, :category_id, :content, :user_id)";
+        try {
+            $stmt = $this->connection->prepare($sql);
+            if($stmt->execute([
+                ":title" => $cours->__get("title"),
+                ":subtitle" => $cours->__get("subtitle"),
+                ":description" => $cours->__get("description"),
+                ":image" => $cours->__get("image"),
+                ":langues" => $cours->__get("langues"),
+                ":type" => $cours->__get("type"),
+                ":category_id" => $cours->__get("category"),
+                ":content" => $cours->__get("document"),
+                ":user_id" => $cours->__get("user_id")
+            ]))
+            return $this->connection->lastInsertId();
+            else return false;
+        } catch (Exception $e) {
+            throw new Exception("Cours creation failed: " . $e->getMessage());
+        }
+    }
+
 }
