822f57365e8a32d3c9f69c1b3072f37a9583a6a0
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-01-18 13:13:07 +0100

    #7 Add properties and magic methods to Enrollement class for user and course management

--- src/class/Enrollement.php
+++ src/class/Enrollement.php
@@ -1,5 +1,20 @@
 <?php
+namespace App\class;
 
 class Enrollement {
-    
+    private int $cours_id;
+    private int $user_id;
+
+    public function __construct($user_id, $cours_id){
+        $this->user_id = intval($user_id);
+        $this->cours_id = intval($cours_id);
+    }
+
+    public function __get($name){
+        return $this->$name;
+    }
+
+    public function __set($name, $value){
+        $this->$name = $value;
+    }
 }
