256a1b4fd095c50e654c9b9c364bed7245e9c8f1
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-01-18 12:42:56 +0100

    #12 Create function get a cours by its ID

--- src/model/CoursModel.php
+++ src/model/CoursModel.php
@@ -24,4 +24,20 @@ class CoursModel {
             throw new Exception("failed to retrieve courses: " . $e->getMessage());
         }
     }
+
+    public function getCourseById($cours_id) {
+        if($cours_id == "") return null;
+        $sql = "SELECT c.*, firstname, lastname, email, gender, specialite, academic_level, category_name, c.category_id
+                FROM courses c
+                JOIN users u ON u.user_id = c.user_id
+                JOIN categories ca ON ca.category_id = c.category_id
+                AND cours_id = :cours_id";
+        try {
+            $stmt = $this->connection->prepare($sql);
+            $stmt->execute([":cours_id" => $cours_id]);
+            return $stmt->fetch();
+        } catch (Exception $e) {
+            throw new Exception("failed to get cours: " . $e->getMessage());
+        }
+    }
 }
