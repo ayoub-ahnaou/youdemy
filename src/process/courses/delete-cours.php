28492d283ccfa681974b508075392492e589c3f1
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-01-18 21:49:24 +0100

    #33 Create the functino of delete a cours from database

---
+++ src/process/courses/delete-cours.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\model\CoursModel;
+
+require_once "../../../vendor/autoload.php";
+
+$cours_id = $_GET["cours_id"];
+var_dump($cours_id);
+
+$coursModel = new CoursModel();
+$res = $coursModel->deleteCours($cours_id);
+
+header("location: ../../app/pages/instractors-courses.php");
