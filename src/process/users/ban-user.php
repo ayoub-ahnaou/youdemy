340b51dd124359e8397be18c9c0fdb4707ebb385
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-01-19 15:00:22 +0100

    #35 Add ban user functionality to the admin process

---
+++ src/process/users/ban-user.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\model\AdminModel;
+
+require_once "../../../vendor/autoload.php";
+$user_id = $_GET["user_id"];
+$adminModel = new AdminModel();
+$adminModel->banUser($user_id);
+
+header("location: ../../app/pages/list-students.php");
