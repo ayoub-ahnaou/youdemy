a5fd251d09c9f4b1a0695375b15b257aee1daeb5
ayoub-ahnaou <ayoubahnaou@gmail.com>
2025-01-19 22:27:52 +0100

    #39 Add one more section to display the total enrollements to admin

--- src/app/components/admin-stats-top.php
+++ src/app/components/admin-stats-top.php
@@ -2,7 +2,7 @@
     <div class="bg-white p-6 rounded-lg shadow-sm">
         <div class="flex items-center justify-between">
             <div>
-                <p class="text-gray-600">Total Students</p>
+                <p class="text-gray-600">Students</p>
                 <h3 class="text-2xl font-bold"><?= $countStudents["total_students"] ?></h3>
             </div>
             <div class="bg-blue-100 p-3 rounded-lg">
@@ -16,7 +16,7 @@
     <div class="bg-white p-6 rounded-lg shadow-sm">
         <div class="flex items-center justify-between">
             <div>
-                <p class="text-gray-600">Total Instractors</p>
+                <p class="text-gray-600">Instractors</p>
                 <h3 class="text-2xl font-bold"><?= $totalInstractors["total_instractors"]; ?></h3>
             </div>
             <div class="bg-yellow-100 p-3 rounded-lg">
@@ -30,12 +30,26 @@
     <div class="bg-white p-6 rounded-lg shadow-sm">
         <div class="flex items-center justify-between">
             <div>
-                <p class="text-gray-600">Active Courses</p>
+                <p class="text-gray-600">Courses</p>
                 <h3 class="text-2xl font-bold"><?= $countCourses["total_courses"]; ?></h3>
             </div>
             <div class="bg-green-100 p-3 rounded-lg">
                 <svg class="w-6 h-6 text-green-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+                </svg>
+            </div>
+        </div>
+    </div>
+
+    <div class="bg-white p-6 rounded-lg shadow-sm">
+        <div class="flex items-center justify-between">
+            <div>
+                <p class="text-gray-600">Enrollements</p>
+                <h3 class="text-2xl font-bold"><?= $countEnrollements["nbre_enrollements"]; ?></h3>
+            </div>
+            <div class="bg-red-100 p-3 rounded-lg">
+                <svg class="w-6 h-6 text-red-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
                 </svg>
             </div>
         </div>
